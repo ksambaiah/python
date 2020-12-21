2bf53b4e77636c597d39aa016902e35f4ffe540d
Sambaiah Kilaru <ksambaiah@gmail.com>
2020-12-21 22:21:23 +0530

    Added bubble sort

---
+++ sorting/bubbleSort.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 12:20:00 2020
+
+@author: samkilar
+"""
+
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(0, len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+
+if __name__ == "__main__":
+    # We are taking array, later we do take some random values
+    arr = [0, 1, -100, -200, -300, 9, 99, 0, 2, 22, 45, -9, -99999]
+    print("Array before sorting")
+    print(arr)
+    arr = bubbleSort(arr)
+    print("Array after sort")
+    print(arr)
