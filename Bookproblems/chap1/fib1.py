9e6e1e73880d162adc5339fec1307866bf4a4d45
Sambaiah Kilaru <ksambaiah@gmail.com>
2021-05-27 23:59:30 +0530

    Added first program from book.

---
+++ Bookproblems/chap1/fib1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+'''
+  Eventhough it is easy program I am writing it to see what -> in
+  function definition.
+  @author
+'''
+
+def fib1(n: int) -> int:
+    if n == 1 or n == 0:
+       return 1 
+    return fib1(n - 1) + fib1(n -2)
+
+  
+if __name__ ==  "__main__":
+    print(fib1(5))
